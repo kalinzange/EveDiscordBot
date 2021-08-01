2625be928cedf1c9bd586172ea7a123258d0cef4
Diogo Santos <diogodeese@gmail.com>
2021-08-01 22:36:53 +0100

    Main.js

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
-asdasd
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = '.';
+const fs = require('fs');
+
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for(const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+
+    client.commands.set(command.name, command);
+}
+
+client.once('ready', () => {
+    console.log('Evee is online');
+});
+ 
+client.on('message', message => {
+
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    switch(command) {
+
+        case 'a': 
+            client.commands.get('a').execute(message, args);
+        break;
+    }
+});
+
+client.login('Nzc1NTMwMzI1NTcyOTc2NjQw.X6nq5g.Zl_HsRXIsRE2rOLc4i4TsX9fYKk');
