a7c1619679e8afcedf59ccb34cb08ef661e01380
kalinzange <38161086+kalinzange@users.noreply.github.com>
2021-08-03 01:59:06 +0100

    Update to Event Handler
    https://www.youtube.com/watch?v=Sihf7B8D4Y8

---
+++ handlers/eventHandler.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+module.exports = (client, Discord) => {
+    const loadDir = (dir) => {
+        const eventFiles = fs.readdirSync(`./events/${dir}`).filter(file => file.endsWith('.js'));
+
+        for (const file of eventFiles) {
+            const event = require(`../events/${dir}/${file}`);
+            const eventName = file.split('.')[0];
+
+            client.on(eventName, event.bind(null, Discord, client));
+        }
+    }
+
+    ['client', 'guild'].forEach( e => loadDir(e));
+}
