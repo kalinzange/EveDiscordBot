fabb87f161b08bd6e1fe0be14f0d447c30f88807
kalinzange <38161086+kalinzange@users.noreply.github.com>
2021-08-03 16:57:51 +0100

    Fix "discord is not defined" Error
    Fix ReferenceError: discord is not defined

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 
 
 client.commands = new Discord.Collection();
-client.events = new discord.Collection();
+client.events = new Discord.Collection();
 
 ['commandHandler', 'eventHandler'].forEach(handler => {
     require(`./handlers/${handler}`)(client, Discord);
