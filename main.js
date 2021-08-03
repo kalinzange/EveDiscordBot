7eb109f33fd26afd5a9906f6fe4f9cc368789779
kalinzange <38161086+kalinzange@users.noreply.github.com>
2021-08-03 01:58:50 +0100

    Update main.js for Event Handler
    https://www.youtube.com/watch?v=Sihf7B8D4Y8

--- main.js
+++ main.js
@@ -1,55 +1,15 @@
 const Discord = require('discord.js');
-const { token, prefix } = require ('./config.json'); 
+const { token } = require ('./config.json'); 
 const client = new Discord.Client();
 const fs = require('fs');
 
 
 client.commands = new Discord.Collection();
-const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
-for(const file of commandFiles) {
-    const command = require(`./commands/${file}`);
+client.events = new discord.Collection();
 
-    client.commands.set(command.name, command);
-}
+['commandHandler', 'eventHandler'].forEach(handler => {
+    require(`./handlers/${handler}`)(client, Discord);
+})
 
-client.once('ready', () => {
-    console.log('Evee is online');
-});
- 
-client.on('message', message => {
 
-    if(!message.content.startsWith(prefix) || message.author.bot) return;
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const command = args.shift().toLowerCase();
-
-    switch(command) {
-
-        case 'a': 
-            client.commands.get('a').execute(message, args);
-        break;
-
-        case 'p':
-            client.commands.get('play').execute(message);
-        break;
-
-        case 'play':
-            client.commands.get('play').execute(message);
-        break;
-
-    }
-});
-/*
-function skip (message, ServerQueue) {
-    if(!message.member.voice.channel) return message.channel.send("Precisas de estar num canal de voz para parar a música!");
-    if(!ServerQueue) return message.channel.send("Não há musicas para dar Skip!")
-    ServerQueue.connection.Dispatcher.end();
-}
-
-function stop (message, ServerQueue) {
-    if(!message.member.voice.channel) return message.channel.send("Precisas de estar num canal de voz para parar a música!");
-    if(!ServerQueue) return message.channel.send("Não há musicas para parar!")
-    ServerQueue.song = [];
-    ServerQueue.connection.Dispatcher.end();
-}
-*/
 client.login(token);
