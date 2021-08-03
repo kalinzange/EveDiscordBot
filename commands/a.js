5ea3154516ff50e46be66559c7205d2d6e62a173
kalinzange <38161086+kalinzange@users.noreply.github.com>
2021-08-03 18:27:55 +0100

    Emoji Test Command

--- commands/a.js
+++ commands/a.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'a',
     description: 'Nothing',
     execute(client, message, args, Discord) {
-        message.channel.send('a');
+        message.channel.send(':mpengu:872164296484868138');
     }
 }
