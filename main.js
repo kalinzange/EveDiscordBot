54009d4542aacb2dc592c781cde991a80b4d19ad
Diogo Santos <diogodeese@gmail.com>
2021-08-04 04:38:55 +0100

    Little adjustments

--- main.js
+++ main.js
@@ -2,13 +2,11 @@ const Discord = require('discord.js');
 const { token } = require ('./config.json'); 
 const client = new Discord.Client();
 
-
 client.commands = new Discord.Collection();
 client.events = new Discord.Collection();
 
 ['commandHandler', 'eventHandler'].forEach(handler => {
     require(`./handlers/${handler}`)(client, Discord);
 })
-
 
 client.login(token);
