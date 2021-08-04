d2a738cb6e768c2370929c7898adf1174cc5f855
Diogo Santos <diogodeese@gmail.com>
2021-08-04 03:56:54 +0100

    Resume not working

--- commands/play.js
+++ commands/play.js
@@ -139,6 +139,6 @@ const pauseSong = (message, serverQueue) => {
 
 const resumeSong = (message, serverQueue) => {
     if(!serverQueue.connection.dispatcher.paused) return message.channel.send("Música não está pausada!");
-    serverQueue.connection.dispatcher.play(song);
+    serverQueue.connection.dispatcher.resume();
     message.channel.send("Rock n' Roll! :D");
 }
