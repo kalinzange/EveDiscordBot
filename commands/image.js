1779b3239e711d61f0c6fc5ddef5e2f80062db9b
Diogo Santos <diogodeese@gmail.com>
2021-08-04 04:56:14 +0100

    Create Image Command

---
+++ commands/image.js
@@ -0,0 +1,19 @@
+const Scraper = require('images-scraper');
+
+const google = new Scraper({
+    puppeteer: {
+        headless: true
+    }
+})
+
+module.exports = {
+    name: 'image',
+    description: 'Searches for an image',
+    async execute(message, args, client, Discord) {
+        const imageQuery = args.join(' ');
+        if(!imageQuery) return message.channel.send('Escolhe uma imagem');
+
+        const imageResults = await google.scrape(imageQuery, 1);
+        message.channel.send(imageResults[0].url);
+    }
+}
