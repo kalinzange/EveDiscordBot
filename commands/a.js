d544ad811c7b2e359dcc4d23cb2b087c0208c81c
kalinzange <38161086+kalinzange@users.noreply.github.com>
2021-08-05 02:31:39 +0100

    Update a.js

--- commands/a.js
+++ commands/a.js
@@ -6,3 +6,10 @@ module.exports = {
         message.channel.send(`${emoji}`);
     }
 }
+
+/* 
+ * Usar Emojis Globally
+ * const emoji = client.emojis.cache.get("871833279609839626")
+ * message.channel.send(`${emoji}`);
+ * 
+*/
