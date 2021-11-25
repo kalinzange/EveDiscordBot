e6746c6dc8116fab7cea964774f8c49cb9e9df44
Diogo Santos <diogodeese@gmail.com>
2021-11-25 04:34:31 +0000

    Ping embed message

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,17 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageEmbed } = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("ping")
     .setDescription("Replies with pong!"),
   async execute(interaction) {
-    await interaction.reply("Pong!");
+    await interaction.reply("Pinging...");
+    const latency = Date.now() - interaction.createdTimestamp;
+    const embed = new MessageEmbed()
+      .setColor("#0099ff")
+      .setTitle("🏓  Pong")
+      .setDescription("Latency is " + latency + "ms.");
+    await interaction.editReply({ content: " ", embeds: [embed] });
   },
 };
