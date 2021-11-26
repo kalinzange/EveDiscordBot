0698b7a2186505a44c6eab2d3d406a5edaf4b455
Diogo Santos <diogodeese@gmail.com>
2021-11-26 01:26:33 +0000

    Added new intents

--- main.js
+++ main.js
@@ -3,9 +3,16 @@ const fs = require("fs");
 const { Client, Intents, Collection } = require("discord.js");
 
 const client = new Client({
-  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_VOICE_STATES,
+  ],
 });
 
+module.exports = client;
+
+// Command Handler
 const commandFiles = fs
   .readdirSync("./commands")
   .filter((file) => file.endsWith(".js"));
@@ -19,6 +26,7 @@ for (const file of commandFiles) {
   client.commands.set(command.data.name, command);
 }
 
+// Event Handler
 const eventFiles = fs
   .readdirSync("./events")
   .filter((file) => file.endsWith(".js"));
